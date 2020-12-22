33d6babc3da1c141c1da9cfbeca9620c0a27e58d
Jeremias ramirez <jereramirez499@gmail.com>
2020-12-22 11:06:26 -0400

    mejorando button de menu

--- src/components/menu/Menu.component.jsx
+++ src/components/menu/Menu.component.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Button from '@material-ui/core/Button'
 import {NavLink} from 'react-router-dom'
 import './Menu.component.css';
-import TextField from '@material-ui/core/TextField';
+ 
 
 
 const MenuComponent = ()=>{
 
-    return <nav className="menu__movie">
-        <ul>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <Button> <NavLink className="link" to="./shows">shows</NavLink> </Button>
-            <Button> <NavLink className="link" to="./start">tv</NavLink> </Button>
+    return <nav className="menu__movie ">
+         <input type="search" className="text__field animate" placeholder="Buscar"/>
+        <ul>  
+            <Button> <NavLink activeClassName="active" className="link animate" to="./shows">shows</NavLink> </Button>
+            <Button> <NavLink className="link animate" to="./start">tv</NavLink> </Button>
         </ul>
     </nav> 
 
