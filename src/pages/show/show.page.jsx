1c5254af1ceebccdced73273dfa7be4378630327
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-08 03:00:25 -0400

    creando banner de movie

--- src/pages/show/show.page.jsx
+++ src/pages/show/show.page.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import './show.page.css';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Button from '@material-ui/core/Button';
+import {NavLink} from 'react-router-dom';
+
 
 const ShowPage = ()=>{
-    const {id} = useParams()
-    return <h1>show {id}</h1>
+    // const {id} = useParams()
+
+    return <>
+        <section className="banner__movie animate">
+        <NavLink to="/shows"><Button className="back"><ArrowBackIosIcon/></Button></NavLink>
+            <article className="banner__elements">
+             
+                <h1 className="banner__title">Titulo</h1>
+            </article>
+
+        </section>
+
+    </>
 }
 
 
